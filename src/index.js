e840980da027bdf12537a8627d60db1b1ebb1144
FabioSC <fabioscarvalh@yahoo.com.br>
2023-01-27 04:42:55 -0300

    Request 4

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
-const HTTP_NOT_STATUS = 404;
+const BAD = 400;
+const NOT = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar.
@@ -23,6 +24,12 @@ const readData = async () => {
   return result;
 };
 
+const regex = (email) => {
+  const regexEmail = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]+$/;
+  const result = regexEmail.test(email);
+  return result;
+};
+
 app.get('/talker', async (req, res) => {
   const read = await readData();
   return res.status(HTTP_OK_STATUS).json(read);
@@ -35,12 +42,25 @@ app.get('/talker/:id', async (req, res) => {
   if (number) {
     return res.status(HTTP_OK_STATUS).json(number);
   }
-  return res.status(HTTP_NOT_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+  return res.status(NOT).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
 app.post('/login', (req, res) => {
+  const { email, password } = req.body;
   const partA = Math.random().toString(16).substring(2);
   const partB = Math.random().toString(16).substring(2);
   const token = (partA + partB).substring(0, 16);
+  if (!email) {
+    return res.status(BAD).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!regex(email)) {
+    return res.status(BAD).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return res.status(BAD).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(BAD).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
   return res.status(HTTP_OK_STATUS).json({ token });
 });
